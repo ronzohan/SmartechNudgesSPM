d89534c0b6a85fd2604ed1815cdad02c8ce83cf1
Ron Daryl Magno <ron.zohan@gmail.com>
2022-06-07 16:36:35 +0800

    Update library targets to include SmartechNudges

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SmartechNudgesSPM",
-            targets: ["SmartechNudgesSPM"]),
+            targets: ["SmartechNudgesSPM", "SmartechNudges"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
